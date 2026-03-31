797aa415b457867b32c6469cc0bbebb426ff7620
王鑫123wangxin <1418667580@qq.com>
2026-03-31 23:03:36 +0800

    feat(article_factory): 为文章工厂添加随机分类关联
    在文章工厂中随机选择一个分类作为关联，而不是固定使用 categoryId 1

--- database/factories/article_factory.ts
+++ database/factories/article_factory.ts
@@ -1,12 +1,14 @@
 import factory from '@adonisjs/lucid/factories'
 import Article from '#models/article'
+import Category from '#models/category'
 
 export const ArticleFactory = factory
   .define(Article, async ({ faker }) => {
+    const category = await Category.query().orderByRaw('rand()').limit(1).first()
     return {
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
-      categoryId: 1,
+      categoryId: category?.id,
     }
   })
   .build()
