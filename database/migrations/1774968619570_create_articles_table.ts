7af803745ff86b1547e06c1f9bdb378f584e11dd
王鑫123wangxin <1418667580@qq.com>
2026-03-31 22:57:30 +0800

    feat: 添加文章相关功能基础结构
    创建文章模型、控制器、数据库迁移和种子数据
    添加分类种子数据用于测试

---
+++ database/migrations/1774968619570_create_articles_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'articles'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title', 255).notNullable().comment('文章标题')
+      table.text('content').notNullable()
+      table
+        .integer('category_id')
+        .nullable()
+        .unsigned()
+        .references('id')
+        .inTable('categories')
+        .onDelete('SET NULL')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
